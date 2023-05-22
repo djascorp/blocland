f1385bd901724f50cfc51a2db01cafdc52b19d34
djasnive <djasniveyrajaona@gmail.com>
2023-05-22 05:36:47 +0300

    [ADD]: create base three column design layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './modules/invoice/presentation/pages/home';
+import { DesignPage } from './modules/invoice/presentation/pages/design';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <p className=''>Hello World</p>
-      </div>
-      <div>
-        <HomePage />
-      </div>
-    </div>
+    <DesignPage />
   );
 }
 
---
+++ src/modules/invoice/presentation/pages/design.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ThreeColumnLayout from "../layouts/three_column_layout"
+
+export const DesignPage:React.FC = () => {
+    return (
+        <>
+        <ThreeColumnLayout ></ThreeColumnLayout>
+        </>
+    )
+}
